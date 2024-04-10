4d17ba5ca62b13ff4a67301c134ad5353b32e7d9
Stefan Zaharie <stefan@zaha.tech>
2024-04-11 00:48:46 +0300

    oops

--- src/main/java/com/zaha/catalog/controllers/TeacherController.java
+++ src/main/java/com/zaha/catalog/controllers/TeacherController.java
@@ -3,13 +3,13 @@ package com.zaha.catalog.controllers;
 import com.zaha.catalog.domain.dto.TeacherDto;
 import com.zaha.catalog.services.TeacherService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Service
+@RestController
 public class TeacherController {
 
     private final TeacherService teacherService;
